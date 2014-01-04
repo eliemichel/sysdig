52861b4aa198d9077e08bb6de291d17fd792082b
Elie Michel <elie.michel@ens.fr>
2014-01-04 15:20:18 +0100

    Copié-collés de code remplacés par des fonctions, c'est plus joli

--- output/interface.py
+++ output/interface.py
@@ -104,409 +104,81 @@ asl = 60
 ix2 = 150
 colon = 30
 
-# J1
+
+def disp_int(ix, iy, mask) :
+	A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
+	B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
+	C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
+	D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
+	E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
+	F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
+	G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
+
+	lineA = Can.create_line(A)
+	lineB = Can.create_line(B)
+	lineC = Can.create_line(C)
+	lineD = Can.create_line(D)
+	lineE = Can.create_line(E)
+	lineF = Can.create_line(F)
+	lineG = Can.create_line(G)
+
+	if (int(mask[0]) == 1) : lineA = Can.create_polygon(A)
+	if (int(mask[1]) == 1) : lineB = Can.create_polygon(B)
+	if (int(mask[2]) == 1) : lineC = Can.create_polygon(C)
+	if (int(mask[3]) == 1) : lineD = Can.create_polygon(D)
+	if (int(mask[4]) == 1) : lineE = Can.create_polygon(E)
+	if (int(mask[5]) == 1) : lineF = Can.create_polygon(F)
+	if (int(mask[6]) == 1) : lineG = Can.create_polygon(G)
+
+def disp_colon(ix, iy) :
+	Can.create_oval(ix,iy+2*e+2*l, ix+e,iy+e+2*l, fill = "black")
+
+def disp_slash(ix, iy) :
+	Can.create_polygon(ix,iy+3*e+2*l, ix+asl,iy, ix+asl+e,iy, ix+e,iy+3*e+2*l)
+
+# Affichage
 
 ix = 40
 iy = 10
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(j1[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(j1[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(j1[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(j1[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(j1[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(j1[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(j1[6]) == 1) : lineG = Can.create_polygon(G)
-
-# J2
-
+disp_int(ix, iy, j1)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(j2[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(j2[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(j2[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(j2[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(j2[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(j2[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(j2[6]) == 1) : lineG = Can.create_polygon(G)
-
+disp_int(ix, iy, j2)
 ix += 90
-aslash1 = Can.create_polygon(ix,iy+3*e+2*l, ix+asl,iy, ix+asl+e,iy, ix+e,iy+3*e+2*l)
-# M1
-
+aslash1 = disp_slash(ix, iy)
 ix += s-e
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(m1[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(m1[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(m1[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(m1[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(m1[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(m1[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(m1[6]) == 1) : lineG = Can.create_polygon(G)
-
-# M2
-
+disp_int(ix, iy, m1)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(m2[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(m2[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(m2[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(m2[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(m2[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(m2[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(m2[6]) == 1) : lineG = Can.create_polygon(G)
-
+disp_int(ix, iy, m2)
 ix += 90
-aslash2 = Can.create_polygon(ix,iy+3*e+2*l, ix+asl,iy, ix+asl+e,iy, ix+e,iy+3*e+2*l)
-# A1
-
+aslash2 = disp_slash(ix, iy)
 ix += s-e
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(a1[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(a1[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(a1[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(a1[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(a1[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(a1[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(a1[6]) == 1) : lineG = Can.create_polygon(G)
-
-# A2
-
+disp_int(ix, iy, a1)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(a2[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(a2[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(a2[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(a2[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(a2[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(a2[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(a2[6]) == 1) : lineG = Can.create_polygon(G)
-
-# A3
-
+disp_int(ix, iy, a2)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(a3[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(a3[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(a3[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(a3[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(a3[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(a3[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(a3[6]) == 1) : lineG = Can.create_polygon(G)
-
-# A4
-
+disp_int(ix, iy, a3)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(a4[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(a4[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(a4[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(a4[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(a4[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(a4[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(a4[6]) == 1) : lineG = Can.create_polygon(G)
-
-
-# H1
+disp_int(ix, iy, a4)
 iy += 3*l
 ix = ix2
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(h1[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(h1[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(h1[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(h1[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(h1[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(h1[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(h1[6]) == 1) : lineG = Can.create_polygon(G)
-
-# H2
-
+disp_int(ix, iy, h1)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(h2[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(h2[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(h2[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(h2[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(h2[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(h2[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(h2[6]) == 1) : lineG = Can.create_polygon(G)
-
+disp_int(ix, iy, h2)
 ix += s
-colon11 = Can.create_oval(ix,iy+2*e+2*l, ix+e,iy+e+2*l, fill = "black")
-colon12 = Can.create_oval(ix,iy+2*e+l, ix+e,iy+e+l, fill = "black")
-# Mn1
-
+colon11 = disp_colon(ix, iy)
+colon12 = disp_colon(ix, iy)
 ix += colon
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(mn1[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(mn1[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(mn1[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(mn1[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(mn1[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(mn1[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(mn1[6]) == 1) : lineG = Can.create_polygon(G)
-
-# Mn2
-
+disp_int(ix, iy, mn1)
 ix += s
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(mn2[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(mn2[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(mn2[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(mn2[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(mn2[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(mn2[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(mn2[6]) == 1) : lineG = Can.create_polygon(G)
-
+disp_int(ix, iy, mn2)
 ix += s
-colon21 = Can.create_oval(ix,iy+2*e+2*l, ix+e,iy+e+2*l, fill = "black")
-colon22 = Can.create_oval(ix,iy+2*e+l, ix+e,iy+e+l, fill = "black")
-# S1
-
+colon21 = disp_colon(ix, iy)
+colon22 = disp_colon(ix, iy)
 ix += colon
-
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(s1[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(s1[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(s1[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(s1[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(s1[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(s1[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(s1[6]) == 1) : lineG = Can.create_polygon(G)
-
-# S2
-
+disp_int(ix, iy, s1)
 ix += s
+disp_int(ix, iy, s2)
 
-A = ix+e,iy, ix+e+l,iy, ix+e+l,iy+e, ix+e,iy+e, ix+e,iy
-B = ix+e+l,iy+e, ix+2*e+l,iy+e, ix+2*e+l,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e
-C = ix+e+l,iy+2*e+l, ix+2*e+l,iy+2*e+l, ix+2*e+l,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+2*e+l
-D = ix+e,iy+2*e+2*l, ix+e+l,iy+2*e+2*l, ix+e+l,iy+e+2*e+2*l, ix+e,iy+e+2*e+2*l, ix+e,iy+2*e+2*l
-E = ix,iy+2*e+l, ix+e,iy+2*e+l, ix+e,iy+2*e+2*l, ix,iy+2*e+2*l, ix,iy+2*e+l
-F = ix,iy+e, ix+e,iy+e, ix+e,iy+e+l, ix,iy+e+l, ix,iy+e
-G = ix+e,iy+e+l, ix+e+l,iy+e+l, ix+e+l,iy+e+e+l, ix+e,iy+e+e+l, ix+e,iy+e+l
-
-lineA = Can.create_line(A)
-lineB = Can.create_line(B)
-lineC = Can.create_line(C)
-lineD = Can.create_line(D)
-lineE = Can.create_line(E)
-lineF = Can.create_line(F)
-lineG = Can.create_line(G)
-
-if (int(s2[0]) == 1) : lineA = Can.create_polygon(A)
-if (int(s2[1]) == 1) : lineB = Can.create_polygon(B)
-if (int(s2[2]) == 1) : lineC = Can.create_polygon(C)
-if (int(s2[3]) == 1) : lineD = Can.create_polygon(D)
-if (int(s2[4]) == 1) : lineE = Can.create_polygon(E)
-if (int(s2[5]) == 1) : lineF = Can.create_polygon(F)
-if (int(s2[6]) == 1) : lineG = Can.create_polygon(G)
 
 Can.pack()
 But = tk.Button(window, text ="Quit", command = window.quit)
